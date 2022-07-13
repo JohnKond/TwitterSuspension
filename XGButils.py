413a7213aa78ffd5db24f8fb6e6c7fc26f8f15e3
JohnKond <martinrekkles99@gmail.com>
2022-07-13 19:44:40 +0300

    fixed xgb finetuning

--- XGButils.py
+++ XGButils.py
@@ -1,24 +1,25 @@
-import numpy as np
-from sklearn import svm
-from sklearn import metrics
 from sklearn.model_selection import GridSearchCV
 from sklearn.model_selection import StratifiedKFold
-from sklearn.ensemble import RandomForestRegressor
 import xgboost as xgb
 
 
 def xgb_finetuning(X_train, y_train):
     print('XGBoost finetuning')
     model = xgb.XGBClassifier(
+        # predictor='gpu_predictor',
+        # tree_method='gpu_hist',
+        objective='binary:logistic',
         use_label_encoder=False
     )
     param_grid = {
-        'n_estimators': [400, 700, 1000],
-        'colsample_bytree': [0.7, 0.8],
-        'max_depth': [15, 20, 25],
+        'n_estimators': [300, 400, 500, 600, 700],
+        'colsample_bytree': [0.65, 0.7, 0.75, 0.8, 0.9],
+        'max_depth': [6, 7, 8, 9, 10, 11, 12, 13, 14, 15],
+        'gamma': [0, 0.25, 0.5, 1.0],
         'reg_alpha': [1.1, 1.2, 1.3],
-        'reg_lambda': [1.1, 1.2, 1.3],
-        'subsample': [0.7, 0.8, 0.9]
+        'reg_lambda': [0.1, 1, 1.1, 1.2, 1.3],
+        'subsample': [0.6, 0.65, 0.7, 0.75, 0.8],
+        'learning_rate': [0.005, 0.01, 0.015]
     }
 
     cv = StratifiedKFold(n_splits=10, shuffle=True)      # 10 folds
@@ -47,15 +48,23 @@ def xgb_run(params, X_train, y_train):
     reg_alpha = params['reg_alpha']
     reg_lambda = params['reg_lambda']
     subsample = params['subsample']
+    learning_rate = params['learning_rate']
+    gamma = params['gamma']
 
     clf = xgb.XGBClassifier(
+        predictor='gpu_predictor',
+        tree_method='gpu_hist',
+        objective='binary:logistic',
         n_estimators=n_estimators,
         colsample_bytree=colsample_bytree,
         max_depth=max_depth,
         reg_alpha=reg_alpha,
         reg_lambda=reg_lambda,
         subsample=subsample,
-        use_label_encoder=False
+        gamma=gamma,
+        learning_rate=learning_rate,
+        use_label_encoder=False,
+        n_jobs=-1
     )
 
     clf.fit(X_train, y_train)
