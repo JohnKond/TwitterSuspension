1e6a831b4a7764064047749052fddd820cea0939
JohnKond <martinrekkles99@gmail.com>
2022-08-25 18:33:32 +0300

    fixed featureSelectionUtils

--- pipeline/featureSelectionUtils.py
+++ pipeline/featureSelectionUtils.py
@@ -4,6 +4,8 @@ import sys
 import pandas as pd
 import numpy as np
 import collections
+import os
+import time
 
 from sklearn.linear_model import Lasso
 from sklearn.model_selection import StratifiedKFold
@@ -43,7 +45,7 @@ def significant_features(X_train, y_train, alpha):
 
 
 # perform feature selection on train dataset
-def feature_selection(X_train, y_train):
+def feature_selection_train(X_train, y_train):
     number_of_folds = 10
     kfold = StratifiedKFold(n_splits=number_of_folds, shuffle=True)
 
@@ -64,4 +66,53 @@ def feature_selection(X_train, y_train):
     alpha = select_best_alpha(lasso_dict, number_of_folds)
 
     return significant_features(X_train, y_train, alpha=alpha)
+
+
+
+''' 
+    Perform feature selection if not done, and return new 
+    train and test files. If feature selection was previously 
+    implemented, load the features and return the datasets.
+'''
+def feature_selection(X_train, y_train, X_test, y_test, path):
+    print('Path : ', path)
+    # if feature selection is implemented import features
+    if os.path.exists(path + 'FSfeatures/features.txt'):
+        print('Importing features from file..')
+        with open(path + '/FSfeatures/features.txt') as f:
+            features = f.readlines()
+        features = [x.strip() for x in features]
+
+    else:
+
+        fs_start = time.time()
+        # returns most significant features
+        features = feature_selection_train(X_train, y_train)
+        fs_end = time.time()
+        print('Feature selection time: ', fs_end - fs_start, ' seconds')
+
+        # store features in file
+        if not os.path.exists(path + 'FSfeatures'):
+            os.mkdir(path + 'FSfeatures')
+
+        with open(path + 'FSfeatures/features.txt', 'w+') as fp:
+            for item in features[1:]:
+                # write each item on a new line
+                fp.write("%s\n" % item)
+
+    print('Feature selection.....Done')
+
+    # X_train and X_test after feature selection
+    X_train = X_train[features]
+    X_test = X_test[features]
+
+    # example dataset
+    # X_train = X_train.iloc[:100]
+    # y_train = y_train.iloc[:100]
+    # X_test = X_test.iloc[:99]
+    # y_test = y_test.iloc[:99]
+
+    return X_train, y_train, X_test, y_test
+
+
 
--- pipeline/model.py
+++ pipeline/model.py
@@ -4,19 +4,19 @@ import os.path
 import pandas as pd
 import time
 
-import featureSelectionUtils
+from featureSelectionUtils import feature_selection
 from SaveLoadUtils import save_params, load_params, save_scores
 from modelSelection import ModelSelection
 from modelTrain import ModelTrain
 from modelPredict import ModelPredict
 
 
-
 # change train_input_folder to your folder path that contains train.tsv and test.tsv
 period = 'feb_mar'
 train_input_folder = '/Storage/gkont/model_input/'
+
 # windows (put in comments)
-#train_input_folder = 'C:/Users/giankond/Documents/thesis/Project/data/'
+# train_input_folder = 'C:/Users/giankond/Documents/thesis/Project/data/'
 
 path = os.path.abspath(os.path.join(os.getcwd(), os.pardir)) + '/'
 
@@ -38,55 +38,9 @@ def read_input():
     
     return X_train, y_train, X_test, y_test
 
-''' 
-    Perform feature selection if not done, and return new 
-    train and test files. If feature selection was previously 
-    implemented, load the features and return the datasets.
-'''
-def feature_selection(X_train, y_train, X_test, y_test):
-    print('Path : ', path)
-    # if feature selection is implemented import features
-    if os.path.exists(path + 'FSfeatures/features.txt'):
-        print('Importing features from file..')
-        with open(path + '/FSfeatures/features.txt') as f:
-            features = f.readlines()
-        features = [x.strip() for x in features]
-
-    else:
-
-        fs_start = time.time()
-        # returns most significant features
-        features = featureSelectionUtils.feature_selection(X_train, y_train)
-        fs_end = time.time()
-        print('Feature selection time: ', fs_end - fs_start, ' seconds')
-
-        # store features in file
-        if not os.path.exists(path + 'FSfeatures'):
-            os.mkdir(path + 'FSfeatures')
-
-        with open(path + 'FSfeatures/features.txt', 'w+') as fp:
-            for item in features[1:]:
-                # write each item on a new line
-                fp.write("%s\n" % item)
-
-    print('Feature selection.....Done')
-
-    # X_train and X_test after feature selection
-    X_train = X_train[features]
-    X_test = X_test[features]
-
-    # example dataset
-    # X_train = X_train.iloc[:100]
-    # y_train = y_train.iloc[:100]
-    # X_test = X_test.iloc[:99]
-    # y_test = y_test.iloc[:99]
-
-    return X_train, y_train, X_test, y_test
-
 
 def main():
-   
-      
+
     # read input training and test datasets
     print('Reading input files..')
     X_train, y_train, X_test, y_test = read_input()
@@ -94,17 +48,14 @@ def main():
     # perform feature selection and return new train and test dataset
     print('Initiating feature selection..')
     X_train, y_train, X_test, y_test = feature_selection(X_train, y_train, X_test, y_test)
-    
 
-    #print('Initiating model selection..')
+    # print('Initiating model selection..')
     # model selection with training / validation on 1st month data
     # ModelSelection('feb_mar', X_train, y_train, k_folds=5)
-    
 
     # train/save best model and scaler on first month data
     ModelTrain(X_train, y_train, X_test, y_test)
 
-    
     # model predict with data from specified months
     # ModelPredict('feb_apr',train_input_folder,fit=True)
 
